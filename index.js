b45fa656aaaf853755c3d0acb2f90da2b47ffbc8
Alessandra Chardon <achardon@ucla.edu>
2021-09-22 10:19:27 -0400

    commit tree 8bfae1eaa9aaf6717b54ececbeab70d38b18b35a

--- index.js
+++ index.js
@@ -1,13 +1,35 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  //separate word into characters
+  const arrOfCharacters = word.split('')
+  const arrLength = arrOfCharacters.length
+  const numbOfCharactersToMatch = Math.floor(arrLength/2)
+  //check to see if first character equals last character
+  //check to see if second character equals second to last character
+  //continue until there are no more characters
+  for (let i=0; i < numbOfCharactersToMatch; i++) {
+    if (arrOfCharacters[i] === arrOfCharacters[arrLength - 1 - i]) {
+      //console.log('letters match')
+    }
+    //if any of the above is false, return false
+    else {
+      return false
+    }
+  }
+  //if everything is true, return true
+  return true
 }
+
+isPalindrome('treehouse')
 
 /* 
   Add your pseudocode here
+  See above!
 */
 
 /*
   Add written explanation of your solution here
+  I need to take an input of a string, divdie it into characters, and then check if the beginning and end of the string match, followed by the second and second to last, etc. If they all match, the function should return true. If not, the function should return false.
 */
 
 // You can run `node index.js` to view these console logs
